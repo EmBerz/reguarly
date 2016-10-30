af48b0886fe25c7e5d54ef368732f4bf7d8abb2f
Emily Berk <Niobe@Emilys-MacBook-Pro.local>
2016-10-30 15:14:12 -0700

    Initial commit

---
+++ app.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import ReactDOM from 'react-dom'
+import $ from 'jquery'
+import Reguarly from './src/reguarly'
+
+ReactDOM.render(
+  <Reguarly></Reguarly>,
+  document.getElementById('reguarly')
+);
