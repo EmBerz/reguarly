7c6d7ac8cb79f1581aa38677699ca1fa53cc19f2
M <Niobe@Emilys-MacBook-Pro.local>
2016-11-17 23:33:39 -0800

    Got mocha tests working and tested the button

--- test/button.spec.js
+++ test/button.spec.js
@@ -1,12 +1,40 @@
-//import { expect } from 'chai';
+
+import jsdom from 'jsdom'
+import React from 'react';
+import { mount } from 'enzyme';
+import Button from '../src/components/button.js'
+import sinon from 'sinon';
+
 let expect = require('chai').expect
-//let Button = require('../src/components/button.js');
+
+const doc = jsdom.jsdom('<!doctype html><html><body></body></html>')
+global.document = doc
+global.window = doc.defaultView
 
 describe('the button component', () => {
+  let button, fakeClicker;
   beforeEach(() => {
-    console.log('button')
+    fakeClicker = sinon.spy();
+    button = mount(<Button label="hello" handleClick={fakeClicker}/>)
   })
-  it('is a button', () =>{
-    expect(true).to.equal(true)
+  it('is not undefined', () => {
+    expect(button).to.not.equal(undefined)
   });
+  it('has the label hello', () => {
+    expect(button.text()).to.equal('hello')
+  });
+  it('calls the click handler on click', () => {
+    button.simulate('click');
+    expect(fakeClicker.called).to.equal(true)
+  })
+
 })
+
+//
+// describe('<Foo />', () => {
+//   it('allows us to set props', () => {
+//     const wrapper = mount(<Foo bar="baz" />);
+//     expect(wrapper.props().bar).to.equal('baz');
+//     wrapper.setProps({ bar: 'foo' });
+//     expect(wrapper.props().bar).to.equal('foo');
+//   });
--- test/test.js
+++
@@ -1,8 +0,0 @@
-var expect = require('chai').expect
-describe('Array', function() {
-  describe('#indexOf()', function() {
-    it('should return -1 when the value is not present', function() {
-      expect(-1).equal([1,2,3].indexOf(4));
-    });
-  });
-});
